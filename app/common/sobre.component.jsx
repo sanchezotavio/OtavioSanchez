f55919dba941cffbe2e6a3b7919537336547e58d
Otavio <Otavio>
2016-12-01 17:06:18 -0200

    atualização sobre

--- app/common/sobre.component.jsx
+++ app/common/sobre.component.jsx
@@ -3,19 +3,19 @@ import React, { Component } from 'react';
 var SobreSection = React.createClass({
     render: function () {
         return (
-            <section className="sobre border" id="sobre">
+            <section className="sobre bg border" id="sobre">
                 <header className="sobre__header">
                     <h2 className="sobre__title">Quem Sou</h2>
                     <span className="division"></span>
                 </header>
                 <div className="container">
                     <div className="quem_sou display-flex">
-                        <div className="o-col-md-12 o-col-xs-4">
-                            <figure className="sobre__figure">
+                        <div className="o-col-md-12 o-col-xs-3">
+                            <figure className="sobre__figure--alter">
                                 <img src="styles/images/otavio-sanchez.jpg" className="sobre__image" alt="Otávio Sanchez" />
                             </figure>
                         </div>
-                        <div  className="o-col-md-12 o-col-xs-8">
+                        <div  className="o-col-md-12 o-col-xs-9">
                             <article id="quemsou">
                                 <p className="paragraph">
                                     Olá, me chamo Otávio Sanchez.Sou Desenvolvedor Web, entusiasta de tecnologia, autodidata e apaixonado por código.
@@ -27,58 +27,55 @@ var SobreSection = React.createClass({
                                 <Skills />
 
                             </article>
+
+                            <hr className="hr" />
+
+                            <header className="sobre__item">
+                                <h2 className="item__title">Profissional</h2>
+                            </header>
+                            <div  className="sobre__item">
+                                <Profissional />
+                            </div>
+
+                            <hr className="hr" />
+
+                            <header className="sobre__item">
+                                <h2 className="item__title">Academico</h2>
+                            </header>
+                            <div  className="sobre__item">
+                                <Academico />
+                            </div>
+
+                            <hr className="hr" />
+
+                            <header className="sobre__item">
+                                <h2 className="item__title">Cursos</h2>
+                            </header>
+                            <div  className="sobre__item">
+                                <Cursos />
+                            </div>
+
+                            <hr className="hr" />
+
+                            <header className="sobre__item">
+                                <h2 className="item__title">Certificações</h2>
+                            </header>
+                            <div  className="sobre__item">
+                                <CursosAleatorios />
+                            </div>
+
+                            <hr className="hr" />
+
+                            <header className="sobre__item">
+                                <h2 className="item__title">Outros Cursos</h2>
+                            </header>
+                            <div  className="sobre__item">
+                                <CursosAleatorios />
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="container display-flex">
-                    <div className="o-col-md-12 o-col-xs-4">
 
-                    </div>
-                    <div  className="o-col-md-12 o-col-xs-8">
-                        <header className="o-col-xs-12">
-                            <h2 className="title">Profissional</h2>
-                        </header>
-                        <div  className="o-col-xs-12">
-                            <Profissional />
-                        </div>
-
-                        <hr />
-
-                        <header className="o-col-xs-12">
-                            <h2 className="title">Academico</h2>
-                        </header>
-                        <div  className="o-col-xs-12">
-                            <Academico />
-                        </div>
-
-                        <hr />
-
-                        <header className="o-col-xs-12">
-                            <h2 className="title">Cursos</h2>
-                        </header>
-                        <div  className="o-col-xs-12">
-                            <Cursos />
-                        </div>
-
-                        <hr />
-
-                        <header className="o-col-xs-12">
-                            <h2 className="title">Certificações</h2>
-                        </header>
-                        <div  className="o-col-xs-12">
-                            <CursosAleatorios />
-                        </div>
-
-                        <hr />
-
-                        <header className="o-col-xs-12">
-                            <h2 className="title">Outros Cursos</h2>
-                        </header>
-                        <div  className="o-col-xs-12">
-                            <CursosAleatorios />
-                        </div>
-                    </div>
-                </div>
             </section>
         );
     }
@@ -91,8 +88,8 @@ var Academico = React.createClass({
             <ul className="academico">
                 {academico.map(function (l) {
                     return <li className="cursos__item">
-                        <h1 className="sobre__title">{l.nome}</h1>
-                        <h2 className="sobre__title--sub">{l.instituicao}</h2>
+                        <h1 className="title">{l.nome}</h1>
+                        <h2 className="title--sub">{l.instituicao}</h2>
                         <p className="text">{l.descricao}</p>
                         <div className="data">{l.data}</div>
                     </li>
@@ -122,8 +119,8 @@ var Profissional = React.createClass({
             <ul className="profissional">
                 {profissional.map(function (l) {
                     return <li className="cursos__item">
-                        <h1 className="sobre__title">{l.nome}</h1>
-                        <h2 className="sobre__title--sub">{l.instituicao}</h2>
+                        <h1 className="title">{l.nome}</h1>
+                        <h2 className="title--sub">{l.instituicao}</h2>
                         <p className="text">{l.descricao}</p>
                         <div className="data">{l.data}</div>
                     </li>
@@ -140,8 +137,8 @@ var Cursos = React.createClass({
             <ul className="cursos">
                 {cursos.map(function (l) {
                     return <li className="cursos__item">
-                        {l.nome}
                         <ul className="cursos__list">
+                            <li className="cursos__sub title">{l.nome}</li>
                             <li className="cursos__sub">{l.instituicao}</li>
                             <li className="cursos__sub">{l.data}</li>
                         </ul>
@@ -152,15 +149,36 @@ var Cursos = React.createClass({
     }
 });
 
+var Certificações = React.createClass({
+    render: function () {
+        return (
+            <ul className="cursos">
+                {certificacoes.map(function (l) {
+                    return <li className="cursos__item">
+
+                        <ul className="cursos__list">
+                            <li className="cursos__sub title">{l.nome}</li>
+                            <li className="cursos__sub">{l.instituicao}</li>
+                            <li className="cursos__sub">{l.data}</li>
+                            <li className="cursos__sub">{l.link}</li>
+                        </ul>
+                    </li>
+                }) }
+            </ul>
+        );
+    }
+});
+
+
 var CursosAleatorios = React.createClass({
     render: function () {
         return (
             <ul className="cursos">
                 {cursosAleatorios.map(function (l) {
                     return <li className="cursos__item">
-                        {l.nome}
                         <ul className="cursos__list">
-                            <li className="cursos__sub">{l.instituicao}</li>
+                            <li className="cursos__sub title"> {l.nome}</li>
+                            <li className="cursos__sub ">{l.instituicao}</li>
                             <li className="cursos__sub">{l.data}</li>
                         </ul>
                     </li>
@@ -177,7 +195,6 @@ class Sobre extends Component {
         return (<SobreSection />);
     }
 }
-
 
 var cursos = [
     { nome: 'Senac', data: 'auuuii aui', instituicao: '' },
@@ -199,10 +216,13 @@ var profissional = [
     { nome: 'Senac', data: 'auuuii aui', empresa: '', descricao: 'asdjsisaji' }
 ];
 
-
 var skills = [
-    { nome: 'HTML', nivel: 1 },
-    { nome: 'CSS', nivel: 2 }
+    { nome: 'HTML', nivel: 8},
+    { nome: 'CSS', nivel: 6 }
 ];
+
+var certificacoes = [
+    { nome: 'Microsoft', data: 'auuuii aui', instituicao: '', link: 'auuuii aui' }
+]
 
 export default Sobre
